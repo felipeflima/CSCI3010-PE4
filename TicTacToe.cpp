652583887d1f15952f2121516aabac6889769b8f
Felipe <felipeflima.2000@gmail.com>
2020-09-24 16:38:15 -0300

    minor changes to board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -21,7 +21,9 @@ std::string SquareTypeStringify(SquareType sq){
 }
 
 void DisplayBoard(const Board &b){
+    std::cout << "   1  2  3" << std::endl;
     for (int i = 0; i < 3; i ++){
+        std::cout << i+1 << " ";
         for (int j = 0; j < 3; j++){
             std::cout << SquareTypeStringify(b.squares[i][j]) + " ";
         }
@@ -36,16 +38,20 @@ void GetPlayerChoice(Board &b){
     std::cout << "Where would you like to play?" << std::endl;
     std::cout << "Row: ";
     std::cin >> b.row;
+    b.row--;
     while (b.row < 0 || b.row > 2){
-        std::cout << "Not a valid square. Please enter a number from 0 to 2 and somewhere not played yet." << std::endl;
+        std::cout << "Not a valid square. Please enter a number from 1 to 3 and somewhere not played yet." << std::endl;
         std::cin >> b.row;
+        b.row--;
     }
     std::cout << std::endl;
     std::cout << "Column: ";
     std::cin >> b.col;
+    b.col--;
     while (b.col < 0 || b.col > 2){
-        std::cout << "Not a valid square. Please enter a number from 0 to 2 and somewhere not played yet." << std::endl;
+        std::cout << "Not a valid square. Please enter a number from 1 to 3 and somewhere not played yet." << std::endl;
         std::cin >> b.col;
+        b.col--;
     }
 }
 
