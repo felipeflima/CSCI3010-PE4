a26bc793ec13aa9826554edddc154c8b1e1d8614
Felipe <felipeflima.2000@gmail.com>
2020-09-24 16:15:07 -0300

    resolved conflicts for DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -37,36 +37,6 @@ void CreateBoard (Board &board){
     }
 }
 
-std::string SquareTypeStringify(SquareType sq){ /*FULLY IMPLEMENTED AND WORKING*/
-    if (sq == SquareType::X )
-    {
-        return "❌";
-    }
-    else if (sq == SquareType::O )
-    {
-        return "⭕️";
-    }
-    else
-    {
-        return "⬛️";
-    }
-}
-
-void DisplayBoard(Board &board){
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            std::cout<< SquareTypeStringify(board.squares[i][j]);
-            if (j<2)
-            {
-                std::cout<<"  |  ";
-            }
-        }
-        std::cout<<std::endl;
-    }
-}
-
 int main (){
     Board *board = new Board();
     CreateBoard(*board);
