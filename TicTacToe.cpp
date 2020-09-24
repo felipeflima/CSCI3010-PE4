02ae469392fd01dd67f7436cc8d99aba71126cfb
Felipe <felipeflima.2000@gmail.com>
2020-09-24 11:11:41 -0300

    adding DisplayBoard() to person1-Felipe

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,6 +8,27 @@ struct Board{
     int col;
 };
 
+std::string SquareTypeStringify(SquareType sq){
+    std::string empty = "\U00002B1C";
+    std::string x = "\U0000274C"; // or "\xE2\x9D\x8C"
+    std::string circle = "\U00002B55";
+
+	switch(sq){
+		case SquareType::X: return x; break; //if the SquareType passed into the function is x , return string representsation of x
+		case SquareType::Empty: return empty; break;
+        case SquareType::O: return circle; break;
+	}
+}
+
+void DisplayBoard(const Board &b){
+    for (int i = 0; i < 3; i ++){
+        for (int j = 0; j < 3; j++){
+            std::cout << SquareTypeStringify(b.squares[i][j]) + " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 void CreateBoard (Board &board){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
@@ -19,4 +40,5 @@ void CreateBoard (Board &board){
 int main (){
     Board *board = new Board();
     CreateBoard(*board);
+    DisplayBoard(*board);
 }
